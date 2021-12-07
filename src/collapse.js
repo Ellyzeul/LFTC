8520aa1019461e722fda2fdd1bfbddbf87ba3d59
Ellyzeul <gabrielaug.240@gmail.com>
2021-12-07 03:28:33 -0300

    feat: Criando collapses

---
+++ src/collapse.js
@@ -0,0 +1,16 @@
+const coll = document.getElementsByClassName("collapsible")
+let content
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active")
+        content = this.nextElementSibling
+
+        if (content.style.display === "block") {
+            content.style.display = "none"
+            return
+        }
+
+        content.style.display = "block"
+    })
+}
