7eca98d82e8548814f181fed6ae40d2155b3a046
yingyangtongxue <daniel.n.monteiro@unesp.br>
2021-12-09 17:06:12 -0300

    Adicionando botão de adicionar nó

--- src/diagram.js
+++ src/diagram.js
@@ -12,7 +12,7 @@ function init(){
 
   myDiagram = $(go.Diagram, "canvas");
 
-  myDiagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
+  // myDiagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
 
   myDiagram.nodeTemplate = 
       $(go.Node, "Auto",
@@ -36,39 +36,15 @@ function init(){
       );
 }
 
-/*
-function load(){
-  myDiagram = $(go.Diagram, "canvas");
 
+function reload(){
   myDiagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
-
-  myDiagram.nodeTemplate = 
-      $(go.Node, "Auto",
-          $(go.Shape, "Circle", { fill: "yellow"},
-              new go.Binding("fill", "color")
-          ),
-          $(go.TextBlock, "text", {margin: 10},
-              new go.Binding("text", "key")
-          )
-      );
-
-  myDiagram.linkTemplate = 
-      $(go.Link,
-          $(go.Shape, {strokeWidth: 3},
-              new go.Binding("stroke", "color")
-          ),
-
-          $(go.Shape, {toArrow: "Standard", stroke: null},
-              new go.Binding("fill", "color")
-          )
-      );
 }
-*/
+
 
 function addElement(){
   let key = "q";
   key = key + String(nodeDataArray.length);
   nodeDataArray.push({ key: key, color: "yellow" });
-  console.log(nodeDataArray);
-  //load();
+  reload();
 }
