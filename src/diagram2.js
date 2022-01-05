b3131dd2447bb873a97f4246619f7590f499ba2f
Ellyzeul <gabrielaug.240@gmail.com>
2022-01-04 21:41:15 -0300

    feat: Atualizando nome e chave dos nós em nodeDataArray

--- src/diagram2.js
+++ src/diagram2.js
@@ -5,20 +5,25 @@ let linkDataArray = [
   //{to: "q2", from: "q1", color: "red", text: "b"}
 ];
 
+const nodeId = () => {
+  if(typeof nodeId.id == 'undefined') nodeId.id = 0
+  
+  return nodeId.id++
+}
+
 let flagConnect = 0;
 
 function init(){
   let $ = go.GraphObject.make;
-  let i = 0;
   
 
   myDiagram = $(go.Diagram, "canvas",
                 {
                     "animationManager.initialAnimationStyle": go.AnimationManager.None,
                     "toolManager.mouseWheelBehavior": go.ToolManager.WheelZoom,
-                    "clickCreatingTool.archetypeNodeData": { 
-                        //key: "",//"q"+ String(nodeDataArray.length),
-                        text: "new"
+                    "clickCreatingTool.archetypeNodeData": {
+                        text: `new`,
+                        key: -1
                     },
                     "undoManager.isEnabled": true,
                     click: function(e) { 
@@ -36,35 +41,32 @@ function init(){
             shadowOffset: new go.Point(0, 1),
             shadowColor: "rgba(0, 0, 0, .14)"
         },
-            new go.Binding("location", "loc", go.Point.parse).makeTwoWay(go.Point.stringify),   
-
-          $(go.Shape, "Circle", { 
-              fill: "yellow",
-              portId: "",
-              fromLinkable: true, fromLinkableSelfNode: true, fromLinkableDuplicates: true,
-              toLinkable: true, toLinkableSelfNode: true, toLinkableDuplicates: true,
-              cursor: "pointer"
-            },
-              new go.Binding("fill", "color")
-          ),
-          $(go.TextBlock, "text", {
-              margin: 10,
-              editable: true
-            },
-              new go.Binding("text", "text") //, "key"
-          ),
-
-          {
-            click: function(e, obj) { 
-              let nodeClicked = obj.part.data.key;
-              console.log("Clicked on " + nodeClicked); 
-            },
-            selectionChanged: function(part) {
-                let shape = part.elt(0);
-                shape.fill = part.isSelected ? "red" : "yellow";
-              }
-          }   
-
+        new go.Binding("location", "loc", go.Point.parse).makeTwoWay(go.Point.stringify),
+        $(go.Shape, "Circle", { 
+            fill: "yellow",
+            portId: "",
+            fromLinkable: true, fromLinkableSelfNode: true, fromLinkableDuplicates: true,
+            toLinkable: true, toLinkableSelfNode: true, toLinkableDuplicates: true,
+            cursor: "pointer"
+          },
+          new go.Binding("fill", "color")
+        ),
+        $(go.TextBlock, "text", {
+            margin: 10,
+            editable: true
+          },
+          new go.Binding("text", "text")
+        ),
+        {
+          click: function(e, obj) { 
+            let nodeClicked = obj.part.data.key;
+            console.log("Clicked on " + nodeClicked); 
+          },
+          selectionChanged: function(part) {
+              let shape = part.elt(0);
+              shape.fill = part.isSelected ? "red" : "yellow";
+            }
+        }
       );
 
   myDiagram.linkTemplate = 
@@ -96,6 +98,22 @@ function init(){
               new go.Binding("text").makeTwoWay())
           )
       );
+
+  myDiagram.toolManager.clickCreatingTool.doMouseUp = () => {
+    var clickTool = myDiagram.toolManager.clickCreatingTool
+    var nodeArray = myDiagram.model.nodeDataArray
+    var a = clickTool.diagram
+    var node
+
+    clickTool.insertPart(a.lastInput.documentPoint)
+    node = nodeArray.pop()
+    node.key *= -1
+    node.text = `q${node.key}`
+    nodeArray.push(node)
+    console.log(nodeArray)
+
+    clickTool.stopTool()
+  }
 }
 
 
@@ -131,5 +149,5 @@ function connectElement(){
 };*/
 
 function diagramToJson(){
-  return myDiagram.model.toJson();
+  return JSON.parse(myDiagram.model.toJson())
 }
