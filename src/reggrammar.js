894fc9104ef3eb931a1ad57bbf1e33244cf9bc55
yingyangtongxue <daniel.n.monteiro@unesp.br>
2022-01-05 18:29:44 -0300

    Botões de adicionar e remover colunas na tabela

--- src/reggrammar.js
+++ src/reggrammar.js
@@ -1,4 +1,19 @@
 function addRow(){
-    let table = document.getElementsByTagName("table");
+    let table = document.getElementById("table-grammar");
+    let row = document.createElement('tr');
+    let html = "<td>" + "<input type='text'>"+ "</td><td>" + "→" + "</td><td>" + "<input type='text'>"+ "</td>";
+    row.innerHTML = html;
+    table.appendChild(row);
+}
+
+
+function deleteRow(){
+    let table = document.getElementById("table-grammar");
+    table.removeChild(table.lastChild);
+}
+
+
+function textToJson(){
+    let tabble = document.getElementById("table-grammar");
     
 }
