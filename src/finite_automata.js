997b998f2ef3695c0828720f9a5317c94e6c4f3b
yingyangtongxue <daniel.n.monteiro@unesp.br>
2022-01-08 22:49:09 -0300

    feat: possibilidade de inserção de mais de um estado (tabela AF - tableToJsonFA())

--- src/finite_automata.js
+++ src/finite_automata.js
@@ -39,17 +39,23 @@ function tableToJsonFA(){
     let arr = new Array();
     let columns = tableFA.rows[0].cells.length;
     let rows = tableFA.rows.length;
+    let k=0;
 
     for(let i=1;i<columns;i++){
         for(let j=1;j<rows;j++){
             let name = tableFA.rows[j].cells[0].children[0].value.split('*');
             let receives = tableFA.rows[0].cells[i].children[0].value;
-            let goesTo = tableFA.rows[j].cells[i].children[0].value;
+            let goesTo = tableFA.rows[j].cells[i].children[0].value.split(',');
 
             let final = name.length == 2 ? true : false;
-        
 
-            arr.push({"name": name[name.length-1], "receives": receives, "goesTo": goesTo, "final": final});
+            while(k<goesTo.length){
+                k++;
+                arr.push({"name": name[name.length-1], "receives": receives, "goesTo": goesTo[goesTo.length-k], "final": final});
+            };
+
+            k=0;
+            //arr.push({"name": name[name.length-1], "receives": receives, "goesTo": goesTo, "final": final});
         }
     }
 
@@ -67,5 +73,21 @@ function tableToJsonFA(){
     | a | b
 q0  | q1| q1
 *q1 | q1| 
+
+
+[
+  {name:'q0', receives:'a', goesTo:'q1', final:false},
+  {name:'q0', receives:'a', goesTo:'q2', final:false},
+  {name:'q0', receives:'b', goesTo:'q1', final:false},
+  {name:'q1', receives:'a', goesTo:'q2', final:false},
+  {name:'q1', receives:'b', goesTo:'q2', final:false},
+  {name:'q2', receives:'a', goesTo:'q2', final:true},
+]
+
+    |  a  |  b
+q0  |q1,q2| q1
+q1  |  q2 | q2
+*q2 |  q2 |
+
 
 */
